108bdd96a6696f945fdb091d55bdab7eed5dcf5c
enayetullahrasuli <enayetrasuli@gmail.com>
2019-06-30 13:08:28 +0430

    first commit

---
+++ src/main/java/throne/springreacto/spring5webfluxrestfull/Spring5WebfluxRestfullApplication.java
@@ -0,0 +1,13 @@
+package throne.springreacto.spring5webfluxrestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebfluxRestfullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/throne/springreacto/spring5webfluxrestfull/Spring5WebfluxRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package throne.springreacto.spring5webfluxrestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
